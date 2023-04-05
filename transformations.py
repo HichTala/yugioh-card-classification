c06bf1dc2f8973105b955080e60648eb1ea52044
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-05 15:17:01 +0200

    fix transformations ranges and gamma positivity

--- transformations.py
+++ transformations.py
@@ -25,10 +25,11 @@ def data_transforms_no_tensor(img):
     img = T.Resize(50)(img)
     img = T.Resize((300, 200))(img)
 
-    gamma = normalvariate(mu=1, sigma=0.5)
-    gain = normalvariate(mu=1, sigma=2)
-    sat_factor = normalvariate(mu=0.7, sigma=0.3)
-    hue_factor = normalvariate(mu=0, sigma=0.1)
+    gamma = normalvariate(mu=1, sigma=0.3)
+    gamma = gamma if gamma >= 0 else 0
+    gain = normalvariate(mu=1, sigma=1)
+    sat_factor = normalvariate(mu=0.7, sigma=0.1)
+    hue_factor = normalvariate(mu=0, sigma=0.03)
 
     img = T.functional.adjust_gamma(img, gamma=gamma, gain=gain)
     img = T.functional.adjust_saturation(img, saturation_factor=sat_factor)
