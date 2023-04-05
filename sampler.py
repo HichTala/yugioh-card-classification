a7ea7694fdb913b4277f66f3795c77375b755946
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-05 11:53:07 +0200

    Add sampler

---
+++ sampler.py
@@ -0,0 +1,15 @@
+from torch import randperm
+
+
+class EpisodicBatchSampler(object):
+    def __init__(self, n_classes, n_way, n_episodes):
+        self.n_classes = n_classes
+        self.n_way = n_way
+        self.n_episodes = n_episodes
+
+    def __len__(self):
+        return self.n_episodes
+
+    def __iter__(self):
+        for i in range(self.n_episodes):
+            yield randperm(self.n_classes)[:self.n_way]
