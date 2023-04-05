575d65ce57f6d11e24ea9188ed96c9676a73122c
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-05 11:52:07 +0200

    Update dataset

--- card_dataset.py
+++ card_dataset.py
@@ -8,21 +8,23 @@ from torch.utils.data import Dataset
 
 class CardDataset(Dataset):
 
-    def __init__(self, imageFolderDataset, transform=None):
-        self.imageFolderDataset = imageFolderDataset
+    def __init__(self, image_folder_dataset, n_support, n_query, transform=None):
+        self.image_folder_dataset = image_folder_dataset
+        self.n_support = n_support
+        self.n_query = n_query
         self.transform = transform
 
     def __getitem__(self, index):
-
-        img_tuple = self.imageFolderDataset.imgs[index]
+        img_tuple = self.image_folder_dataset.imgs[index]
 
         img = Image.open(img_tuple[0])
         label = img_tuple[1]
 
         if self.transform is not None:
-            img = self.transform(img)
+            support = [self.transform(img) for _ in range(self.n_support)]
+            query = [self.transform(img) for _ in range(self.n_query)]
 
         return img, label
 
     def __len__(self):
-        return len(self.imageFolderDataset.imgs)
+        return len(self.image_folder_dataset.imgs)
