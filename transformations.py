4fbfee13ea1026fdfa2a752f9307b161f31b9e6e
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-05 14:44:17 +0200

    Update datat augmentation/transformation (add random values)

--- transformations.py
+++ transformations.py
@@ -1,43 +1,44 @@
-from torchvision import transforms
+from random import uniform, normalvariate
+
+from torchvision import transforms as T
 
 
 def data_transforms(img):
-    img = transforms.Resize(50)(img)
-    img = transforms.Resize((300, 200))(img)
+    img = data_transforms_no_tensor(img)
 
-    img = transforms.functional.adjust_gamma(img, 0.7, 1.3)
-    img = transforms.functional.adjust_saturation(img, 0.7)
-    img = transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(img)
-    img = transforms.functional.adjust_hue(img, -0.044444)
-
-    img = transforms.ToTensor()(img)
-    img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
+    img = T.ToTensor()(img)
+    img = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
 
     return img
 
 
 def image_transforms(img):
-    img = transforms.Resize((300, 200))(img)
+    img = T.Resize((300, 200))(img)
 
-    img = transforms.ToTensor()(img)
-    img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
+    img = T.ToTensor()(img)
+    img = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
 
     return img
 
 
 def data_transforms_no_tensor(img):
-    img = transforms.Resize(50)(img)
-    img = transforms.Resize((300, 200))(img)
+    img = T.Resize(50)(img)
+    img = T.Resize((300, 200))(img)
 
-    img = transforms.functional.adjust_gamma(img, 0.7, 1.3)
-    img = transforms.functional.adjust_saturation(img, 0.7)
-    img = transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(img)
-    img = transforms.functional.adjust_hue(img, -0.044444)
+    gamma = normalvariate(mu=1, sigma=0.5)
+    gain = normalvariate(mu=1, sigma=2)
+    sat_factor = normalvariate(mu=0.7, sigma=0.3)
+    hue_factor = normalvariate(mu=0, sigma=0.1)
+
+    img = T.functional.adjust_gamma(img, gamma=gamma, gain=gain)
+    img = T.functional.adjust_saturation(img, saturation_factor=sat_factor)
+    img = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(img)
+    img = T.functional.adjust_hue(img, hue_factor=hue_factor)
 
     return img
 
 
 def image_transforms_no_tensor(img):
-    img = transforms.Resize((300, 200))(img)
+    img = T.Resize((300, 200))(img)
 
     return img
