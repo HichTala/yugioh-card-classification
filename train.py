b1148b60b418fed437f0d395350946313ecf70fa
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-05 13:53:11 +0200

    Update train pipeline

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
-from torch import optim, save, load
+import torch.cuda
+from torch import optim, save, load, cat, tensor
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.models import ResNet101_Weights
+from tqdm import tqdm
 
-from sm_dataset import SiameseNetworkDataset
+from card_dataset import CardDataset
+from protonet import ProtoNet
+from prototypical_loss import prototypical_loss as loss_fn
+from sampler import EpisodicBatchSampler
 from siamese_network import SiameseNetwork
 from loss_functions import ContrastiveLoss
 from transformations import data_transforms
@@ -12,69 +17,88 @@ from transformations import data_transforms
 class Config:
     training_dir = "./cardDatabaseFull/"
 
-    train_batch_size = 7
     train_number_epochs = 300
-    resum_training = True
+    resume_training = True
+
+    lr = 0.0005
+
+    n_way = 1000
+    n_episodes = 22
+    n_support = 5
+    n_queries = 5
+
+
+def data_initialization(n_way, n_episodes, n_support, n_queries):
+    folder_dataset = datasets.ImageFolder(root=Config.training_dir)
+    train_dataset = CardDataset(image_folder_dataset=folder_dataset, n_support=n_support, n_queries=n_queries,
+                                transform=data_transforms)
+
+    sampler = EpisodicBatchSampler(len(train_dataset), n_way, n_episodes)
+
+    return DataLoader(train_dataset, sampler=sampler, num_workers=0)
+
+
+def train(model, optimizer, train_dataloader, n_way, n_support, n_queries):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print("Start training")
+    for epoch in range(Config.train_number_epochs):
+        model.train()
+
+        for batch in tqdm(train_dataloader, desc="Epoch {:d} train".format(epoch)):
+            optimizer.zero_grad()
+
+            assert batch['supports'].size(0) == batch['queries'].size(0)
+
+            label = batch['label']
+            supports = batch['supports']
+            queries = batch['queries']
+
+            label = label.view(n_way, 1, 1).expand(n_way, n_queries, 1).long()
+            label = tensor(label, requires_grad=False)
+
+            inputs = cat([
+                supports.view(n_way * n_support, *supports.size()[2:]),
+                supports.view(n_way * n_queries, *queries.size()[2:])
+            ], dim=0)
+
+            outputs = model(inputs)
+
+            loss, results = loss_fn(outputs, label, n_way, n_support, n_queries)
+
+            loss.backward()
+            optimizer.step()
+
+            savePath = './models/checkpoints/res_epoch_{}_012023.pth'.format(epoch)
+            save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'results': results
+            }, savePath)
+
+            epoch += 1
+
+        savePath = './res-300-normalized.pth'
+        save(model.state_dict(), savePath)
 
 
 if __name__ == '__main__':
+    train_dataloader = data_initialization(
+        n_support=Config.n_support,
+        n_queries=Config.n_queries,
+        n_episodes=Config.n_episodes,
+        n_way=Config.n_way
+    )
 
-    folder_dataset = datasets.ImageFolder(root=Config.training_dir)
+    model = ProtoNet()
+    optimizer = optim.Adam(model.parameters(), lr=Config.lr)
 
-    siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
-                                            transform=data_transforms)
-
-    train_dataloader = DataLoader(siamese_dataset,
-                                  shuffle=True,
-                                  batch_size=Config.train_batch_size)
-
-    # net = SiameseNetwork(weights=None).cuda()
-    # net.load_state_dict(load("models/res-withShift-no_para-150-072020.pth"))
-
-    net = SiameseNetwork(ResNet101_Weights.IMAGENET1K_V1).cuda()
-    criterion = ContrastiveLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0005)
-
-    counter = []
-    loss_history = []
-    iteration_number = 0
-    epoch = 0
-
-    if Config.resum_training:
-        state_dict = load("./models/checkpoints/res_epoch_60_012023.pth")
-        epoch = state_dict['epoch']
-        net.load_state_dict(state_dict['model_state_dict'])
-        optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-        loss_history = state_dict['loss']
-
-    while epoch <= Config.train_number_epochs:
-        for i, data in enumerate(train_dataloader):
-
-            input0, input1, label = data
-            input0, input1, label = input0.cuda(), input1.cuda(), label.cuda()
-            optimizer.zero_grad()
-            output1, output2 = net(input0, input1)
-            loss_contrastive = criterion(output1, output2, label)
-            loss_contrastive.backward()
-            optimizer.step()
-
-            if i % 10 == 0:
-                print("Epoch number {} - Iteration number {}\n Current loss {}\n".format(epoch, i,
-                                                                                         loss_contrastive.item()))
-                iteration_number += 10
-                counter.append(iteration_number)
-                loss_history.append(loss_contrastive.item())
-            # Adding a validation set
-
-        savePath = './models/checkpoints/res_epoch_{}_012023.pth'.format(epoch)
-        save({
-            'epoch': epoch,
-            'model_state_dict': net.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss_history
-        }, savePath)
-
-        epoch += 1
-
-    savePath = './res-300-normalized.pth'
-    save(net.state_dict(), savePath)
+    train(
+        model=model,
+        optimizer=optimizer,
+        train_dataloader=train_dataloader,
+        n_way=Config.n_way,
+        n_support=Config.n_support,
+        n_queries=Config.n_queries
+    )
