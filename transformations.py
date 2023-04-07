b455a1b436c4421f6a31b2d96ce4a4c44a1427b8
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-07 13:39:40 +0200

    Adapting some transformations after testing

--- transformations.py
+++ transformations.py
@@ -5,19 +5,19 @@ from torchvision import transforms as T
 
 def train_data_transforms(img):
     # img = T.Resize(60, antialias=True)(img)
-    img = T.Resize((53, 65), antialias=True)(img)
+    img = T.Resize((98, 65), antialias=True)(img)
 
-    gamma = normalvariate(mu=1, sigma=0.3)
+    gamma = normalvariate(mu=1, sigma=0.2)
     gamma = gamma if gamma >= 0 else 0
-    gain = normalvariate(mu=1, sigma=1)
+    gain = normalvariate(mu=1, sigma=0.05)
     sat_factor = normalvariate(mu=0.7, sigma=0.1)
-    hue_factor = normalvariate(mu=0, sigma=0.03)
+    hue_factor = normalvariate(mu=0, sigma=0.018)
 
     img = T.functional.adjust_gamma(img, gamma=gamma, gain=gain)
     img = T.functional.adjust_saturation(img, saturation_factor=sat_factor)
     img = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(img)
     img = T.functional.adjust_hue(img, hue_factor=hue_factor)
-    img = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
+    # img = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
 
     return img
 
