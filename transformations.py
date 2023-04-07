19fc56a4a1c92b8f25ba704a53fe1c26cc1554f0
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-07 14:51:51 +0200

    adjust values and image sizes

--- transformations.py
+++ transformations.py
@@ -5,12 +5,12 @@ from torchvision import transforms as T
 
 def train_data_transforms(img):
     # img = T.Resize(60, antialias=True)(img)
-    img = T.Resize((98, 65), antialias=True)(img)
+    img = T.Resize((95, 63), antialias=True)(img)
 
     gamma = normalvariate(mu=1, sigma=0.2)
     gamma = gamma if gamma >= 0 else 0
-    gain = normalvariate(mu=1, sigma=0.05)
-    sat_factor = normalvariate(mu=0.7, sigma=0.1)
+    gain = normalvariate(mu=1, sigma=0.075)
+    sat_factor = normalvariate(mu=0.7, sigma=0.15)
     hue_factor = normalvariate(mu=0, sigma=0.018)
 
     img = T.functional.adjust_gamma(img, gamma=gamma, gain=gain)
@@ -23,12 +23,12 @@ def train_data_transforms(img):
 
 
 def final_data_transforms(img):
-    img = T.Resize(50, antialias=True)(img)
-    img = T.Resize((75, 50), antialias=True)(img)
+    # img = T.Resize(50, antialias=True)(img)
+    img = T.Resize((95, 63), antialias=True)(img)
 
     img = T.functional.adjust_gamma(img, gamma=1, gain=1)
     img = T.functional.adjust_saturation(img, saturation_factor=0.7)
-    img = T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(img)
+    img = T.GaussianBlur(kernel_size=(5, 9), sigma=2.5)(img)
     img = T.functional.adjust_hue(img, hue_factor=0)
 
     img = T.ToTensor()(img)
@@ -38,7 +38,7 @@ def final_data_transforms(img):
 
 
 def image_transforms(img):
-    img = T.Resize((300, 200), antialias=True)(img)
+    img = T.Resize((98, 65), antialias=True)(img)
 
     img = T.ToTensor()(img)
     img = T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
