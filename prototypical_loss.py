4a46cfb9a2b9abe06f96c8368eba3633808cfdc7
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-11 14:48:32 +0200

    update loss to fit larger batch size

--- prototypical_loss.py
+++ prototypical_loss.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+
 import torch
 from torch.nn import functional as F
 
@@ -20,18 +23,42 @@ def euclidean_dist(x, y):
     return torch.pow(x - y, 2).sum(2)
 
 
-def prototypical_loss(outputs, label, n_classes, n_support, n_queries):
-    dim = outputs.size(-1)
+def dist_computation(n_classes, n_supports):
+    dists = []
 
-    supports = outputs[:n_classes * n_support]
-    queries = outputs[n_classes * n_support:]
+    supports_dir = './models/pickles/supports/pickle_{}'
+    queries_dir = './models/pickles/queries/pickle_{}'
 
-    prototype = supports.view(n_classes, n_support, dim).mean(1)
+    i = 0
+    while True:
+        supports_path = supports_dir.format(i)
+        queries_path = queries_dir.format(i)
 
-    dists = euclidean_dist(queries, prototype)
+        i += 1
+        if os.path.exists(supports_path) and os.path.exists(queries_path):
+            with open(supports_path, "rb") as f:
+                supports = pickle.load(f)
+            f.close()
+
+            dim = supports.size(-1)
+            prototype = supports.view(n_classes, n_supports, dim).mean(1)
+            del supports
+
+            with open(queries_path, "rb") as f:
+                queries = pickle.load(f)
+            f.close()
+
+            dists.append(euclidean_dist(queries, prototype))
+            del queries
+        else:
+            return torch.cat(dists)
+
+
+def prototypical_loss(n_classes, n_supports, n_queries):
+    dists = dist_computation(n_classes, n_supports)
 
     log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_queries, -1)
-
+    label = torch.arange(0, n_classes).view(n_classes, 1, 1).expand(n_classes, n_queries, 1).long().cuda()
     loss_val = -log_p_y.gather(2, label).squeeze().view(-1).mean()
 
     _, y_hat = log_p_y.max(2)
