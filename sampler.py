c77ef59404d6e803ad2ecb487e8925dc8a79cd3f
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-11 14:48:43 +0200

    update sampler to return larger batch size

--- sampler.py
+++ sampler.py
@@ -1,15 +1,16 @@
-from torch import randperm
+import numpy as np
 
 
 class EpisodicBatchSampler(object):
     def __init__(self, n_classes, n_way, n_episodes):
         self.n_classes = n_classes
         self.n_way = n_way
-        self.n_episodes = n_episodes
 
     def __len__(self):
-        return self.n_episodes
+        return int(np.ceil(self.n_classes / self.n_way))
 
     def __iter__(self):
-        for i in range(self.n_episodes):
-            yield randperm(self.n_classes)[:self.n_way]
+        for j in range(0, self.n_classes, self.n_way):
+            if j + self.n_way >= self.n_classes:
+                yield range(j, self.n_classes)
+            yield range(j, j + self.n_way)
