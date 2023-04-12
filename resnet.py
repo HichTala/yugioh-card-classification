e867db7ef643a79d40cf4be021a0fc3f77fc8d70
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-12 11:17:52 +0200

    update to resnet 101

--- resnet.py
+++ resnet.py
@@ -1,16 +1,11 @@
 import torch.nn as nn
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet101, ResNet101_Weights
 
 
 class ResNet(nn.Module):
-    """
-    Model as described in the reference paper,
-    source: https://github.com/jakesnell/prototypical-networks/blob/f0c48808e496989d01db59f86d4449d7aee9ab0c/protonets/models/few_shot.py#L62-L84
-    """
-
     def __init__(self):
         super(ResNet, self).__init__()
-        self.resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.resnet = resnet101(weights=ResNet101_Weights.IMAGENET1K_V2)
 
     def forward(self, x):
         x = self.resnet(x)
