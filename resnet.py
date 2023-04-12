480714dc40a0cd64facc586d6ac6b1ba34a25653
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-12 11:51:31 +0200

    update to resnet 152

--- resnet.py
+++ resnet.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
-from torchvision.models import resnet101, ResNet101_Weights
+from torchvision.models import resnet152, ResNet152_Weights
 
 
 class ResNet(nn.Module):
     def __init__(self):
         super(ResNet, self).__init__()
-        self.resnet = resnet101(weights=ResNet101_Weights.IMAGENET1K_V2)
+        self.resnet = resnet152(weights=ResNet152_Weights.IMAGENET1K_V2)
 
     def forward(self, x):
         x = self.resnet(x)
