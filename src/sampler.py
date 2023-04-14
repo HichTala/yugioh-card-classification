08f737c9a1d06b7864ed4667d19f51ff0ad10946
HichTala <98521878+HichTala@users.noreply.github.com>
2023-04-14 16:49:36 +0200

    adapting sampler to come back to an episodic training

--- src/sampler.py
+++ src/sampler.py
@@ -1,16 +1,16 @@
 import numpy as np
+from torch import randperm
 
 
 class EpisodicBatchSampler(object):
-    def __init__(self, n_classes, n_way):
-        self.n_classes = 4752
+    def __init__(self, n_classes, n_episodes, n_way):
+        self.n_classes = n_classes
+        self.n_episodes = n_episodes
         self.n_way = n_way
 
     def __len__(self):
-        return int(np.ceil(self.n_classes / self.n_way))
+        return self.n_episodes
 
     def __iter__(self):
-        for j in range(0, self.n_classes, self.n_way):
-            if j + self.n_way >= self.n_classes:
-                yield range(j, self.n_classes)
-            yield range(j, j + self.n_way)
+        for _ in range(self.n_episodes):
+            yield randperm(self.n_classes)[:self.n_way]
