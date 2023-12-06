f8ddcae8f31799b6f0b574db78910ed9ec5e55f5
Hich Tala <98521878+HichTala@users.noreply.github.com>
2023-12-06 17:59:57 +0100

    Augment card cropping

--- src/tools.py
+++ src/tools.py
@@ -1,3 +1,3 @@
 def art_cropper(img):
     width, height = img.size
-    return img.crop((int(0.115 * width), int(0.18 * height), int(0.89 * width), int(0.71 * height)))
+    return img.crop((6, 30, width - 6, height - 65))
